237d3de426d4a6f0724da6f8147dd299d2644dfc
Mohamed Hassan <mhass405@gmail.com>
2021-07-07 15:58:23 +0200

    Refactor to use reducer

--- 1480-Running Sum of 1d Array/1480-runningSumOf1DArray.js
+++ 1480-Running Sum of 1d Array/1480-runningSumOf1DArray.js
@@ -3,17 +3,7 @@
  * @return {number[]}
  */
 
-const runningSum = function (nums) {
-  const runningSum = [];
-  let sum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (i === 0) {
-      sum = nums[i];
-      runningSum.push(sum);
-    } else {
-      sum += nums[i];
-      runningSum.push(sum);
-    }
-  }
-  return runningSum;
+const runningSum = (nums) => {
+  nums.reduce((a, c, i, arr) => (arr[i] += a));
+  return nums;
 };
